226568771b570bedfe185b078052ecd21eebcbf0
William Long <will@williamlong.xyz>
2021-07-08 18:04:16 +0000

    Initial commit.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,52 @@ int renown = 0;
 int gold = 0;
 int money_sack = 0;
 int money_limit = 64;
+char inventory[20][20];
 
+class Inventory{
+    private:
+    int itemNumber;
+    int quantity;
+    double cost;
+    double totalCost;
+    public:
+    Inventory(){
+        itemNumber = 0;
+        quantity = 0;
+        cost = 0;
+        totalCost = 0;
+    }
+    Inventory(int newItemNumber, int newQuantity, double newCost){
+        itemNumber = newItemNumber;
+        quantity = newQuantity;
+        cost = newCost;
+        setTotalCost(quantity, cost);
+    }/*
+    void setItemNumber(int){
+        itemNumber = itemNumber;
+    }
+    void setQuantity(int){
+        quantity = quantity;
+    }
+    void setCost(double){
+        cost = cost;
+    }*/
+    int getItemNumber(){
+        return itemNumber;
+    }
+    int getQuantity(){
+        return quantity;
+    }
+    double getCost(){
+        return cost;
+    }
+    double getTotalCost(){
+        return totalCost;
+    }
+    void setTotalCost(int, double){
+        totalCost = quantity * cost;
+    }
+};
 class npc{
     public:
     int age;
@@ -70,8 +115,7 @@ void shorvyyn_encounter(int){
             renown += 5;
             printf("renown: %d\n", renown);
         }
-        *human.current_health -= shorvyyn.attack;
-        take_damage(shorvyyn.attack);
+        take_damage(damage = shorvyyn.attack);
         printf("%d\n", *human.current_health);
         if (*human.current_health <= 0){
             printf("You were not well enough prepared to battle Shorvyyn.\n");
@@ -89,18 +133,35 @@ void goblin_encounter(int){
             renown += 1;
             printf("renown: %d\n", renown);
         }
-        *human.current_health -= 1;
+        take_damage(damage = 1);
         printf("Your current health is: %d\n", *human.current_health);
         if (*human.current_health <= 0){
-            printf("You have been killed.");
+            printf("You have been killed.\n");
             renown -= 5;
             printf("renown: %d\n", renown);
         }
     }
 };
-int main(void) {
+class treasure{
+    public:
+    int price;
+    int value;
+} ruby, emerald, saphire, onyx_sculpture, crucifix;
+class key_relic{
+    public:
+} olive_branch, blood_of_lamb, nail, wood;
+void sell(int value, char item){
+    inventory[10][10] -= item;
+    deposit_gold(value);
+}
+int main(){
+    int itemNumber;
+    int quantity;
+    double cost;
+    double totalCost;
     char name[20];
     bool name_con = true;
+
     while (name_con){
         printf("Enter your name: ");
         scanf("%s", name);
@@ -109,8 +170,43 @@ int main(void) {
     }
     while (run){
         printf("Battle time\n");
-        goblin_encounter(*human.current_health);
+        shorvyyn_encounter(*human.current_health);
         break;
     }
+    Inventory();
+    {
+        itemNumber = 0;
+        quantity = 0;
+        cost = 0;
+        totalCost = 0;
+    }
+    printf("\nEnter an item number. ");
+    scanf("%d", &itemNumber);
+    while (itemNumber < 0){
+        printf("Please enter a positive integer value.");
+        scanf("%d", &itemNumber);
+    }
+    printf("\nEnter a quantity. ");
+    scanf("%d", &quantity);
+    while (quantity < 0){
+        printf("Please enter a positive integer value.");
+        scanf("%d", &quantity);
+    }
+    printf("\nEnter a cost. ");
+    scanf("%lf", &cost);
+    while (cost < 0){
+        printf("Please enter a positive value, with no further precision than the hundredths place. ");
+        scanf("%lf", &cost);
+    }
+    Inventory information(itemNumber, quantity, cost);
+    totalCost = information.getTotalCost();
+    itemNumber = information.getItemNumber();
+    quantity = information.getQuantity();
+    cost = information.getCost();
+    printf("\nItem number: %d\n", itemNumber);
+    printf("Item quantity: %d\n", quantity);
+    printf("Item cost: %lf\n", cost);
+    printf("---------------------------\n");
+    printf("Total cost: %lf\n", totalCost);    
     return 0;
 }
